b58c3b13e6066e705930ff6392126d8fb5a3b496
trenkas <43112306+trenkas@users.noreply.github.com>
2018-09-13 11:41:39 +0300

    Add files via upload

---
+++ HomeTask1.java
@@ -0,0 +1,19 @@
+public class HomeTask1 {
+    public static void main(String[] args){
+        Figure figure1 = new Rectangle(2,9);
+        System.out.println(figure1.toString());
+        Figure figure2 = new Square(4);
+        System.out.println(figure2.toString());
+        Figure figure3 = new Rhombus(2,10);
+        System.out.println(figure3.toString());
+        Circle circle = new Circle(3);
+        System.out.println(circle.toString());
+        Figure triangle = new Triangle(2,3,4,5);
+        System.out.println(triangle.toString());
+
+
+
+
+
+    }
+}
